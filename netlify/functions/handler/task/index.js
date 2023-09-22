df37fa2a945c4805e86849dff36b2d9b85dc6d45
b1994mi <b1994mi@gmail.com>
2023-09-22 22:58:43 +0700

    update delete handler registration

--- netlify/functions/handler/task/index.js
+++ netlify/functions/handler/task/index.js
@@ -1,9 +1,10 @@
 const { Router } = require('express')
 const DI = require('../dependencyInjection')
 const create = require('./create')
+const deleteHandler = require('./delete')
 
 const router = Router()
 router.post('/', create(DI))
-router.delete('/:id', create(DI))
+router.delete('/:id', deleteHandler(DI))
 
 module.exports = router
