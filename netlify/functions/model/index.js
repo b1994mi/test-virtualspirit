a8e8176dc63763a0b50665210c4a4398387605fb
b1994mi <b1994mi@gmail.com>
2023-09-22 22:54:37 +0700

    remove sequelize logging to stdout

--- netlify/functions/model/index.js
+++ netlify/functions/model/index.js
@@ -2,8 +2,7 @@ const { Sequelize, DataTypes } = require('sequelize')
 const pg = require('pg')
 
 const sequelize = new Sequelize(process.env.PG_DSN, {
-    benchmark: true,
-    logging: (s, t) => { console.log(`${s} [${t}ms]`) },
+    logging: false,
     pool: { max: 2, min: 0, idle: 2 },
     define: { underscored: true },
     dialectModule: pg,
