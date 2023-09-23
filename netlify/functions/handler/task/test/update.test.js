9a0446017ae5d06b344ebbaedeee2beb21a2bf88
b1994mi <b1994mi@gmail.com>
2023-09-23 17:25:12 +0700

    add unit tests for update handler

--- netlify/functions/handler/task/test/update.test.js
+++ netlify/functions/handler/task/test/update.test.js
@@ -38,3 +38,182 @@ test('ONLY FOR LOCAL TESTING', { skip: true }, async (t) => {
     const r = await update({ db })(mockReq, mockRes)
     console.log(JSON.stringify(r))
 })
+
+test('valid body is provided', async (t) => {
+    const mockDB = mock.newDB()
+    const mockRes = mock.newRes()
+
+    const mockReq = {
+        params: { id: '2' },
+        body: {
+            title: 'asd',
+            description: 'asd',
+            completed: true,
+        }
+    }
+
+    mockDB.Task = {
+        findOne: () => ({ id: '1' }),
+        update: () => (null),
+    }
+
+    const r = await update({
+        db: mockDB,
+    })(mockReq, mockRes)
+
+    assert.equal(r.statusCode, 200, 'status code is different')
+    assert.deepStrictEqual(r.body, {
+        acknowledge: true
+    }, "must contain exact same body for success")
+})
+
+test('title is not present in body', async (t) => {
+    const mockDB = mock.newDB()
+    const mockRes = mock.newRes()
+
+    const mockReq = {
+        params: { id: '2' },
+        body: {
+            description: 'asd',
+            completed: true,
+        }
+    }
+
+    const r = await update({
+        db: mockDB,
+    })(mockReq, mockRes)
+
+    assert.equal(r.statusCode, 400, 'status code is different')
+    assert.deepStrictEqual(r.body, {
+        error: 'title must be present and must be a string'
+    }, "must contain exact same body for bad request")
+})
+
+test('description is not present in body', async (t) => {
+    const mockDB = mock.newDB()
+    const mockRes = mock.newRes()
+
+    const mockReq = {
+        params: { id: '2' },
+        body: {
+            title: 'asd',
+            completed: true,
+        }
+    }
+
+    const r = await update({
+        db: mockDB,
+    })(mockReq, mockRes)
+
+    assert.equal(r.statusCode, 400, 'status code is different')
+    assert.deepStrictEqual(r.body, {
+        error: 'description must be present and must be a string'
+    }, "must contain exact same body for bad request")
+})
+
+test('completed is not a boolean', async (t) => {
+    const mockDB = mock.newDB()
+    const mockRes = mock.newRes()
+
+    const mockReq = {
+        params: { id: '2' },
+        body: {
+            title: 'asd',
+            description: 'asd',
+            completed: "true",
+        }
+    }
+
+    const r = await update({
+        db: mockDB,
+    })(mockReq, mockRes)
+
+    assert.equal(r.statusCode, 400, 'status code is different')
+    assert.deepStrictEqual(r.body, {
+        error: 'completed must be boolean'
+    }, "must contain exact same body for bad request")
+})
+
+test('db.Task.findOne throws an exception', async (t) => {
+    const mockDB = mock.newDB()
+    const mockRes = mock.newRes()
+
+    const mockReq = {
+        params: { id: '2' },
+        body: {
+            title: 'asd',
+            description: 'asd',
+            completed: true,
+        }
+    }
+
+    const error = new Error('exceptionMsg')
+    mockDB.Task = {
+        findOne: () => (Promise.reject(error)),
+    }
+
+    const r = await update({
+        db: mockDB,
+    })(mockReq, mockRes)
+
+    assert.equal(r.statusCode, 422, 'status code is different')
+    assert.deepStrictEqual(r.body, {
+        error
+    }, "must contain exact same body for unprocessable entity")
+})
+
+test('task not found', async (t) => {
+    const mockDB = mock.newDB()
+    const mockRes = mock.newRes()
+
+    const mockReq = {
+        params: { id: '2' },
+        body: {
+            title: 'asd',
+            description: 'asd',
+            completed: true,
+        }
+    }
+
+    mockDB.Task = {
+        findOne: () => (null),
+    }
+
+    const r = await update({
+        db: mockDB,
+    })(mockReq, mockRes)
+
+    assert.equal(r.statusCode, 404, 'status code is different')
+    assert.deepStrictEqual(r.body, {
+        error: "task not found"
+    }, "must contain exact same body for not found")
+})
+
+test('db.Task.update throws an exception', async (t) => {
+    const mockDB = mock.newDB()
+    const mockRes = mock.newRes()
+
+    const mockReq = {
+        params: { id: '2' },
+        body: {
+            title: 'asd',
+            description: 'asd',
+            completed: true,
+        }
+    }
+
+    const error = new Error('exceptionMsg')
+    mockDB.Task = {
+        findOne: () => ({ id: '1' }),
+        update: () => (Promise.reject(error)),
+    }
+
+    const r = await update({
+        db: mockDB,
+    })(mockReq, mockRes)
+
+    assert.equal(r.statusCode, 422, 'status code is different')
+    assert.deepStrictEqual(r.body, {
+        error
+    }, "must contain exact same body for unprocessable entity")
+})
